33588b8703313ec9f1f8132342ab9962732611f3
Timon Borter <timon.borter@gmx.ch>
2023-04-23 17:50:21 +0200

    chore: read existing users before rotation

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,9 @@ extern crate config;
 extern crate hashicorp_vault;
 extern crate postgres;
 
+use std::fs::File as FsFile;
+use std::process::exit;
+
 use config::Config;
 use config::File;
 use hashicorp_vault::client::VaultClient;
@@ -10,8 +13,38 @@ use rand::distributions::{Alphanumeric, DistString};
 use reqwest::blocking::Client as HttpClient;
 use reqwest::header::HeaderMap;
 use serde_json::json;
-use std::fs::File as FsFile;
-use std::process::exit;
+
+fn get_existing_users_from_postgres(prefix: &str, config: &Config) -> Result<Vec<String>, Box<dyn std::error::Error>> {
+    // TODO: Could be [database] block
+    let database_url = config.get_string("database_url")?;
+
+
+    // Connect to PostgreSQL and execute a SELECT query
+    // TODO: Use one connection through whole lifecycle
+    let mut client = PostgresClient::connect(&database_url, NoTls)?;
+
+    let result = client.query(
+        &format!("SELECT username FROM users WHERE username LIKE '{}%'", prefix),
+        &[],
+    );
+
+    // Example code to query PostgreSQL and retrieve existing users
+    let mut existing_users = Vec::new();
+
+    match result {
+        Ok(rows) => {
+            for row in &rows {
+                let username: String = row.get("username");
+                existing_users.push(username);
+            }
+        }
+        Err(err) => {
+            println!("Failed to retrieve existing users from PostgreSQL database: {}", err);
+        }
+    }
+
+    Ok(existing_users)
+}
 
 fn generate_username(prefix: &str, length: usize) -> String {
     let random_part = Alphanumeric.sample_string(&mut rand::thread_rng(), length);
@@ -52,7 +85,7 @@ fn write_to_vault(username: &str, password: &str, secret_path: &str, config: &Co
         "username": username,
         "password": password
     });
-    client.set_secret(secret_path,  secret_data.to_string())?;
+    client.set_secret(secret_path, secret_data.to_string())?;
     Ok(())
 }
 
@@ -79,6 +112,12 @@ fn trigger_argocd_sync(config: &Config) -> Result<(), Box<dyn std::error::Error>
     Ok(())
 }
 
+fn delete_users_from_postgres(users: Vec<String>, config: &Config) {
+    // Perform PostgreSQL delete operation here
+    println!("Deleting users from PostgreSQL database...");
+    // Example implementation: Connect to PostgreSQL, execute delete query for each user
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Check if ".propellerrc" file exists
     let config_path = ".propellerrc";
@@ -91,9 +130,11 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // TODO: use `ConfigBuilder` instead
     config.merge(File::with_name(&config_path))?;
 
+    let mut existing_users: Vec<String> = Vec::new();
+
     let username_map = config.get_array("secrets")?; // Read username map from configuration
     for secret in username_map {
-        let secret_config = match  secret.into_table(){
+        let secret_config = match secret.into_table() {
             Ok(cfg) => cfg,
             Err(err) => {
                 println!("Failed to load configuration: {}", err);
@@ -104,6 +145,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         // TODO: `unwrap` is unsafe!
         let prefix = secret_config.get("prefix").unwrap().to_string();
         let secret_path = secret_config.get("vault_path").unwrap().to_string();
+
+        for existing_username in get_existing_users_from_postgres(&prefix, &config)? {
+            existing_users.push(existing_username);
+        }
 
         let username_length = config.get_int("username_length")? as usize;
         let username = generate_username(&prefix, username_length);
@@ -116,7 +161,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         match write_to_vault(&username, &password, &secret_path, &config) {
             Ok(()) => {
                 println!("Stored username and password for prefix '{}' in Vault secret '{}'", prefix, secret_path);
-            },
+            }
             Err(e) => {
                 println!("Failed to store username and password for prefix '{}' in Vault secret '{}': {}", prefix, secret_path, e);
             }
@@ -124,6 +169,11 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     }
 
     trigger_argocd_sync(&config)?;
+
+    // Delete users from PostgreSQL database if any existing users were found
+    if !existing_users.is_empty() {
+        delete_users_from_postgres(existing_users, &config);
+    }
 
     Ok(())
 }
