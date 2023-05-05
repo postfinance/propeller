6af6a907f9c344669006e99a9f613a985fc2aa08
Timon Borter <timon.borter@gmx.ch>
2023-05-05 16:24:32 +0200

    chore: exchange workflow stub

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,14 @@ use clap::Parser;
 use lazy_static::lazy_static;
 
 use crate::internal::argocd::ArgoCDClient;
-use crate::internal::config::{load_config, Args};
-use crate::internal::database::postgres::PostgresClient;
+use crate::internal::config::{Args, load_config, Secret};
 use crate::internal::database::DatabaseClient;
+use crate::internal::database::postgres::PostgresClient;
 use crate::internal::vault::VaultClient;
-use crate::internal::workflow::rotate::RotateWorkflow;
+use crate::internal::workflow;
 use crate::internal::workflow::{Workflow, WorkflowKind};
+use crate::internal::workflow::exchange::ExchangeWorkflow;
+use crate::internal::workflow::rotate::RotateWorkflow;
 
 mod internal;
 
@@ -26,8 +28,6 @@ fn main() {
     let postgres = PostgresClient::new(&config.database);
     let vault = VaultClient::new(&config.vault);
 
-    let mut workflow;
-
     match CLI_ARGS
         .workflow
         .to_string()
@@ -35,14 +35,19 @@ fn main() {
         .expect(
             format!(
                 "🛑 Invalid workflow '{}', allowed values are: exchange, rotate!",
-                CLI_ARGS                    .workflow
+                CLI_ARGS.workflow
             )
             .as_str(),
         ) {
-        WorkflowKind::EXCHANGE => workflow = RotateWorkflow::new(argocd, postgres, vault),
-        WorkflowKind::ROTATE => workflow = RotateWorkflow::new(argocd, postgres, vault),
+        WorkflowKind::EXCHANGE => {
+            let mut workflow = ExchangeWorkflow::new(argocd, postgres, vault);
+            let secrets = workflow.sanitize(config.secrets);
+            workflow.run(secrets);
+        }
+        WorkflowKind::ROTATE => {
+            let mut workflow = RotateWorkflow::new(argocd, postgres, vault);
+            let secrets = workflow.sanitize(config.secrets);
+            workflow.run(secrets);
+        }
     }
-
-    let secrets = workflow.sanitize(config.secrets);
-    workflow.run(secrets);
 }
