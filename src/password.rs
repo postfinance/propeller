f7e23f1f40154b24c1d336ee0bd71ce66d02959d
Timon Borter <timon.borter@gmx.ch>
2024-07-04 10:44:50 +0200

    chore: random password function

---
+++ src/password.rs
@@ -0,0 +1,31 @@
+use rand::distributions::Alphanumeric;
+use rand::{thread_rng, Rng};
+
+fn generate_random_password(length: usize) -> String {
+    let mut rng = thread_rng();
+    let password: String = (0..length)
+        .map(|_| rng.sample(Alphanumeric) as char)
+        .collect();
+    password
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_password_length() {
+        for length in vec![8, 16, 32] {
+            let password = generate_random_password(length);
+            assert_eq!(password.len(), length);
+        }
+    }
+
+    #[test]
+    fn test_password_content() {
+        let password = generate_random_password(10);
+        assert!(password.chars().all(|c| c.is_alphanumeric()));
+        assert!(password.chars().any(|c| c.is_lowercase()));
+        assert!(password.chars().any(|c| c.is_uppercase()));
+    }
+}
