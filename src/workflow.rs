cde4ffc70f2d543346f4b14142996ce8a9eec0a1
Timon Borter <timon.borter@gmx.ch>
2024-07-12 15:57:12 +0200

    fix(clippy): removed code smells

--- src/workflow.rs
+++ src/workflow.rs
@@ -18,7 +18,7 @@ pub(crate) fn rotate_secrets_using_switch_method(
     let vault_path = config.vault.clone().path;
     let mut secret: VaultStructure = vault
         .read_secret()
-        .expect(format!("Failed to read path '{vault_path}' - did you init Vault?").as_str());
+        .unwrap_or_else(|_| panic!("Failed to read path '{vault_path}' - did you init Vault?"));
 
     if secret.postgresql_active_user != secret.postgresql_user_1
         && secret.postgresql_active_user != secret.postgresql_user_2
@@ -52,11 +52,19 @@ pub(crate) fn rotate_secrets_using_switch_method(
 
 fn switch_active_user(secret: &mut VaultStructure) {
     if secret.postgresql_active_user == secret.postgresql_user_1 {
-        secret.postgresql_active_user = secret.postgresql_user_2.clone();
-        secret.postgresql_active_user_password = secret.postgresql_user_2_password.clone()
+        secret
+            .postgresql_active_user
+            .clone_from(&secret.postgresql_user_2);
+        secret
+            .postgresql_active_user_password
+            .clone_from(&secret.postgresql_user_2_password);
     } else {
-        secret.postgresql_active_user = secret.postgresql_user_1.clone();
-        secret.postgresql_active_user_password = secret.postgresql_user_1_password.clone()
+        secret
+            .postgresql_active_user
+            .clone_from(&secret.postgresql_user_1);
+        secret
+            .postgresql_active_user_password
+            .clone_from(&secret.postgresql_user_1_password);
     }
 
     trace!("Switched active and passive user in Vault secret (locally)")
@@ -70,11 +78,11 @@ fn update_passive_user_postgres_password(
     let (passive_user, passive_user_password) =
         if secret.postgresql_active_user == secret.postgresql_user_1 {
             let original_password = secret.postgresql_user_2_password.clone();
-            secret.postgresql_user_2_password = new_password.clone();
+            secret.postgresql_user_2_password.clone_from(&new_password);
             (secret.postgresql_user_2.clone(), original_password)
         } else {
             let original_password = secret.postgresql_user_1_password.clone();
-            secret.postgresql_user_1_password = new_password.clone();
+            secret.postgresql_user_1_password.clone_from(&new_password);
             (secret.postgresql_user_1.clone(), original_password)
         };
 
@@ -82,7 +90,7 @@ fn update_passive_user_postgres_password(
     let query = format!("ALTER ROLE {passive_user} WITH PASSWORD '{new_password}'");
 
     conn.execute(query.as_str(), &[])
-        .expect(format!("Failed to update password of '{passive_user}'").as_str());
+        .unwrap_or_else(|_| panic!("Failed to update password of '{passive_user}'"));
 
     debug!("Successfully rotated PostgreSQL password of passive user");
 }
