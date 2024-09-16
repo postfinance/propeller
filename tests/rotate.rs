157bf8b988b67dc50ced4443fba01b5c81bf96b9
Timon Borter <timon.borter@gmx.ch>
2024-09-16 15:37:07 +0200

    ci: collapse integration tests into one job

--- tests/rotate.rs
+++ tests/rotate.rs
@@ -17,7 +17,6 @@ use utilities::{
 use vaultrs::client::VaultClient;
 use vaultrs::kv2;
 
-#[ignore]
 #[tokio::test(flavor = "multi_thread")]
 async fn rotate_secrets() {
     let (k3s_container, postgres_container, vault_container) =
@@ -124,7 +123,6 @@ async fn rotate_secrets() {
     stop_sender.send(()).expect("Failed to send stop signal");
 }
 
-#[ignore]
 #[tokio::test(flavor = "multi_thread")]
 async fn rotate_application_sync_timeout() {
     let (k3s_container, postgres_container, vault_container) =
